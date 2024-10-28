677e9b07ce731e6d251594f5f8f6b03413663f52
shuvxm <kumarshubham5559@gmail.com>
2024-10-28 12:09:24 +0530

    updated

--- MockProgram/BloodPower.java
+++ MockProgram/BloodPower.java
@@ -2,4 +2,21 @@ import java.util.Arrays;
 
 class BloodPower {
 
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 4, 5, 6, 7, 4, 3, 1 };
+        int a = 0;
+        int b = 0;
+        Arrays.sort(arr);
+        int l = arr.length - 1;
+        for (int i : arr)
+            b += i;
+        while (b >= a) {
+            a = a + arr[l];
+            b = b - arr[l];
+            l--;
+        }
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
