05242ac168c8fa0f07670ba6a02d2ec20352d403
shuvxm <kumarshubham5559@gmail.com>
2024-11-05 09:09:37 +0530

    updated

--- Searching/LinearSearch/MinNumber.java
+++ Searching/LinearSearch/MinNumber.java
@@ -1,13 +1,19 @@
 // find minimum number.
 public class MinNumber {
     public static void main(String[] args) {
-        int[] arr = { 18, 12, 7, 3, 4, 14, 28 };
+        int[] arr = { 18, 12, 7, 3, 4, 14, -28 };
         System.out.println(min(arr));
     }
 
     // return minimum value in array
     static int min(int[] arr) {
-        return 0;
+        int minVal = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minVal)
+                minVal = arr[i];
+        }
+        return minVal;
+
     }
 
 }
